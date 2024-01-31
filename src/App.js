5ccfeeed7f57d4f70e30234f53214513f89c878a
Gassama <gassma803@gmail.com>
2024-01-31 16:32:15 +0100

    installation de redux toolkit car createStore est deprecié

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
+import React,{ useState } from 'react';
+import {useDispatch,useSelector } from 'react-redux';
+import { addTask,deleteTask,toggleTask } from './actions/taskAction';
 
 function App() {
+  const [input,setInput] = useState('');
+  const dispatch = useDispatch();
+  const tasks = useSelector(state => state.tasks);
+
+  const handleAddTask = () => {
+    dispatch(addTask({id:Date.now(),text:input,completed:false}));
+    setInput('');
+  };
+
   return (
     <div>
-      
+      <input value={input} onChange={e => setInput(e.target.value)} />
+      <button onClick={handleAddTask}>Add Task</button>
     </div>
   );
 }
