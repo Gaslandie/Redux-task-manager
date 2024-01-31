db74fb4e1ca1fe1928d3b6b7699fd051a505c2ef
Gassama <gassma803@gmail.com>
2024-01-31 16:20:00 +0100

    mise en place de taskAction et taskReducer

---
+++ src/reducers/taskReducers.js
@@ -0,0 +1,31 @@
+import { ADD_TASK,DELETE_TASK,TOGGLE_TASK } from "../actions/taskAction";
+
+//l'etat initial de nos taches
+const initialState = {
+    tasks : []
+};
+
+//notre fonction reducer pour gerer letat de nos tache
+const tasksReducer = (state=initialState,action) =>{
+    switch (action.type){
+        case ADD_TASK:
+            return{
+                ...state,
+                tasks: [...state.tasks, action.payload]
+            }
+        case DELETE_TASK:
+            return {
+                ...state,
+                tasks: state.tasks.filter(task => task.id !== action.payload)
+            };
+        case TOGGLE_TASK:
+            return {
+                ...state,
+                tasks:state.tasks.map(task => task.id === action.payload ?{...task,completed: !task.completed}:task)
+            };
+            default:
+                return state;
+    }
+}
+
+export default tasksReducer;
