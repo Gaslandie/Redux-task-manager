8be3d19c86bee490ce4c8c88a80a8eec76eb434c
Gassama <gassma803@gmail.com>
2024-01-31 20:23:15 +0100

    meme probleme,toujorus pas resolu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import {useDispatch,useSelector} from 'react-redux';
 import { addTask,deleteTask,toggleTask } from './actions/taskAction';
 
 function App() {
   const [input,setInput] = useState('');
   const dispatch = useDispatch();
-  const tasks = useSelector(state => state.tasks);
+  const tasks = useSelector(state=>{
+    return state.tasks
+  });
+  
+  useEffect(() => {
+    console.log("Tasks a changé :", tasks);
+  }, [tasks]);
+  
 
   const handleAddTask = () => {
     if (input.trim() !== '') {
@@ -28,9 +35,8 @@ function App() {
       placeholder="Ajouter une nouvelle tâche"
     />
     <button onClick={handleAddTask}>Ajouter</button>
-
     <ul>
-      {tasks.map(task => (
+      {Array.isArray(tasks) ? tasks.map(task => (
         <li key={task.id} style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
           {task.text}
           <button onClick={() => handleToggleTask(task.id)}>
@@ -38,7 +44,7 @@ function App() {
           </button>
           <button onClick={() => handleDeleteTask(task.id)}>Supprimer</button>
         </li>
-      ))}
+      )):<p>Tasks n'est pas un tableau</p>}
     </ul>
   </div>
   );
