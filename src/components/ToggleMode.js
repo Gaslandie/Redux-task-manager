0755f0a3d82cbb07cec29636461212a3d60fac53
Gassama <gassma803@gmail.com>
2024-02-01 02:31:27 +0100

    ajout ToggleMode.js

--- src/components/ToggleMode.js
+++ src/components/ToggleMode.js
@@ -1,1 +1,26 @@
 //ce composant est pour le changement de theme de notre page
+import { useState } from "react";
+import { FaMoon,FaSun } from "react-icons/fa";
+
+export default function ToggleMode() {
+    const [darkMode,setDarkMode] = useState(false);
+    const changeTheme = () => {
+        document.body.classList.toggle("dark");
+        setDarkMode(!darkMode);
+    }
+    return (
+        //notre fonction changeTheme est sans parenthèse car on veut pas qu'il soit executé
+        //au rendu mais lorsqu'elle est aplé
+        <section className="toggle" onClick={changeTheme}>
+      {darkMode ? (
+        <section className="toggle-light">
+          <FaSun />
+        </section>
+      ) : (
+        <section className="toggle-dark">
+          <FaMoon />
+        </section>
+      )}
+    </section>
+    )
+}
